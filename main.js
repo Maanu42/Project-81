16cf02ca79ade82816bc9764e5ba94ed92f75de2
Maanu42 <76243940+Maanu42@users.noreply.github.com>
2021-10-22 19:34:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,50 @@
 canvas= document.getElementById("my_canvas");
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle= "silver";
+ctx.lineWidth= 3;
+ctx.rect(0,143,700,400)
+ctx.stroke();
 //Blue
 ctx= canvas.getContext("2d");
 ctx.beginPath();
 ctx.strokeStyle= "blue";
-ctx.lineWidth=2;
-ctx.arc(200,200,100,0,360);
+ctx.lineWidth=5;
+ctx.arc(150,300,100,0,360);
 ctx.stroke();
-function circle(mouse_x,mouse_y){
-ctx.beginPath();
-ctx.stokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,100,0,360);
-ctx.stroke();}
+
 //yellow
 ctx= canvas.getContext("2d");
 ctx.beginPath();
 ctx.strokeStyle= "yellow";
-ctx.lineWidth=2;
-ctx.arc(300,300,100,0,360)
+ctx.lineWidth=5;
+ctx.arc(250,400,100,0,360)
 ctx.stroke();
-function circle(mouse_x,mouse_y){
-ctx.beginPath();
-ctx.stokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,100,0,360);
-ctx.stroke();}
 //black
 ctx= canvas.getContext("2d");
 ctx.beginPath();
 ctx.strokeStyle= "black";
-ctx.lineWidth=2;
-ctx.arc(400,200,100,0,360)
+ctx.lineWidth=5;
+ctx.arc(350,300,100,0,360)
 ctx.stroke();
-function circle(mouse_x,mouse_y){
-ctx.beginPath();
-ctx.stokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,100,0,360);
-ctx.stroke();}
 //Green
 ctx= canvas.getContext("2d");
 ctx.beginPath();
 ctx.strokeStyle= "green";
-ctx.lineWidth=2;
-ctx.arc(500,300,100,0,360)
+ctx.lineWidth=5;
+ctx.arc(450,400,100,0,360)
 ctx.stroke();
-function circle(mouse_x,mouse_y){
-ctx.beginPath();
-ctx.stokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,100,0,360);
-ctx.stroke();}
+
 //Red
 ctx= canvas.getContext("2d");
 ctx.beginPath();
 ctx.strokeStyle= "red";
-ctx.lineWidth=2;
-ctx.arc(600,200,100,0,360)
+ctx.lineWidth=5;
+ctx.arc(550,300,100,0,360)
 ctx.stroke();
-function circle(mouse_x,mouse_y){
+
+//trying
 ctx.beginPath();
-ctx.stokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,100,0,360);
-ctx.stroke();}
+ctx.strokeStyle="pink";
+ctx.lineWidth= 5;
+ctx.arc(250,200,40,0,2*Math.PI)
